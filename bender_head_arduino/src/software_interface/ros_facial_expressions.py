497f05ac9f9c0eeeb16d6f267847193b428367dd
Gustavo Díaz <g.hernan.diaz@gmail.com>
2016-08-26 12:36:32 -0300

    Actualizo tareas pendientes y agrego cmd para joystick

--- bender_head_arduino/src/software_interface/ros_facial_expressions.py
+++ bender_head_arduino/src/software_interface/ros_facial_expressions.py
@@ -15,6 +15,7 @@ from threading import Thread
 
 from std_msgs.msg import Empty
 from bender_head_arduino.msg import ExpressionCommand
+#from bender_head_arduino.msg import Emotion
 
 # Use HW interface
 from head_hw_controller import HeadHWController
@@ -25,7 +26,7 @@ from facial_expressions import FacialExpressions
 #from dynamixel_driver.dynamixel_io import DynamixelIO
 from dynamixel_io import DynamixelIO
 
-DEV_ID = 0
+DEV_ID = 16
 
 class ROSFacialExpressions:
     def __init__(self, dxl_io, controller_namespace, port_namespace):
@@ -40,6 +41,7 @@ class ROSFacialExpressions:
         self.facial_gestures = FacialGestures(self.servos_hw)
         self.facial_expressions = FacialExpressions(self.eyes, self.facial_gestures)
         self.expression_state = ExpressionCommand()
+        #self.joystick_msg = ExpressionCommand()
         
     def initialize(self):
         # Get params and allocate msgs
@@ -52,6 +54,7 @@ class ROSFacialExpressions:
 		#subscribers
         self.command_sub = rospy.Subscriber(self.controller_namespace + '/expression_command', ExpressionCommand, self.process_command)
         self.expressionsList_sub = rospy.Subscriber(self.controller_namespace + '/expression_list', Empty, self.list_expressions)
+        #self.joystick_sub = rospy.Subscriber(self.controller_namespace + '/joystick_face_command', Emotion, self.joystick_cmd)
        #publishers
         self.state_pub = rospy.Publisher(self.controller_namespace + '/expression_state', ExpressionCommand)
         Thread(target=self.update_state).start()
@@ -60,6 +63,7 @@ class ROSFacialExpressions:
         self.running = False
         self.command_sub.unregister()
         self.expressionsList_sub.unregister()
+        #self.joystick_sub.unregister()
         self.state_pub.unregister()
 
     def process_command(self, msg):
@@ -77,12 +81,20 @@ class ROSFacialExpressions:
             self.facial_expressions.default()
         elif (msg.expression == "apagado"):
             self.facial_expressions.apagado()
-            
+        elif (msg.expression == "id"):
+            device_id = self.hw_controller.get_state(3)
+            print("device_id = "+str(device_id))
+
+    def joystick_cmd(self, msg):
+        if (msg.Order == "changeFace"):
+            self.joystick_msg.expression = msg.Action
+            self.command_sub.publish(self.joystick_msg)
+
     def list_expressions(self, msg):
-        print "------------------------------------------------------------"
-        print "Expressions Availables:\n"
-        print "surprised\nangry\nhappy\nsad\nveryHappy\ndefault\napagado\n"
-        print "------------------------------------------------------------"
+        print("------------------------------------------------------------")
+        print("Expressions Availables:\n")
+        print("surprised\nangry\nhappy\nsad\nveryHappy\ndefault\napagado\n")
+        print("------------------------------------------------------------")
 
     def update_state(self):
         rate = rospy.Rate(self.state_update_rate)
@@ -98,7 +110,7 @@ class ROSFacialExpressions:
 
 if __name__ == '__main__':
     rospy.init_node('expressions_controller')
-    dxl = DynamixelIO('/dev/ttyUSB0', baudrate = 115200)
+    dxl = DynamixelIO('/dev/ttyUSB1', baudrate = 115200)
     expressions = ROSFacialExpressions(dxl, 'expressions', 'left')
     expressions.initialize()
     expressions.start()
